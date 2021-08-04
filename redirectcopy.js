1c8173a160ec2f75e58854b3e7a4e6410562a4eb
nguyennhatlong <nhatlongevent@gmail.com>
2021-08-04 21:13:50 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,17 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/> XIN CẢM PHIỀN, NỘI DUNG ĐƯỢC BẢO MẬT TRÁNH SAO CHÉP : <a href='https://www.facebook.com/360batdongsan'>https://www.facebook.com/360batdongsan"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
